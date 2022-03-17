d84d5c6b5968c1b0464473d12a97fd4d77654b86
Aleksey-Abramov <aleksey.abramov22@yandex.ru>
2022-03-17 16:48:45 +0300

    CRIT UPD: Контрольная точка. Все работает.

--- Gemfile
+++ Gemfile
@@ -24,6 +24,10 @@ gem "cssbundling-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+gem "turbo-rails"
+
+gem 'jquery-rails'
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,10 @@
 // Entry point for the build script in your package.json
 import * as bootstrap from "bootstrap"
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+
+Rails.start()
+Turbolinks.start()
+
+//= require rails-ujs
+//= require_tree
